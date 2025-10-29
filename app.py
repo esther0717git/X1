edb3aaef91625bfb78f5ae1f29fa3385e7a573dc
esther0717git <liangwy@sea.com>
2025-10-29 13:27:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown(
 # ---------------------- HEADER ----------------------
 st.markdown("<div class='main-title'> 🚀 FileSmith AI</div>", unsafe_allow_html=True)
 st.markdown(
-    "<div class='subtitle'> Meet Mr. FileSmith - The gentleman unencrypt filenames and do bulk renaming files in a blink of eye. <b>Start Date</b>, <b>End Date</b>, <b>Name</b>, and <b>Order No</b>.</div>",
+    "<div class='subtitle'> Meet Mr. FileSmith — the gentleman who decrypts filenames and performs bulk renaming in the blink of an eye. </div>",
     unsafe_allow_html=True,
 )
 
